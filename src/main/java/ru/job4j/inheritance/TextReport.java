18fb1dd8bd10b052f0e51285f9cde200092c9f92
Dima Sh <dyus79@yandex.ru>
2021-11-16 20:11:58 +0300

    4. Переопределение [#174890]

--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -1,7 +1,7 @@
 package ru.job4j.inheritance;
 
 public class TextReport {
-    public static String generate(String name, String body) {
+    public  String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }
 }
