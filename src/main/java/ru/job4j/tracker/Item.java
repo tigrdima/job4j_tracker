9c363cd10af2dfceab1d4b110dfb54f5b61d1388
Dima Sh <dyus79@yandex.ru>
2021-11-18 17:07:55 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -26,6 +29,10 @@ public class Item {
 
     public String getName() {
         return name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public void setName(String name) {
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item time = new Item();
+        LocalDateTime currentDateTime = time.getCreated();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = currentDateTime.format(formatter);
+        System.out.println(currentDateTimeFormat);
+
+    }
+
+}
