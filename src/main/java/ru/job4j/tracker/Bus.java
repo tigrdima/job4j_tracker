e1641e9ad80fe0ff9a259cf1743193abcb8b46a7
Dima Sh <dyus79@yandex.ru>
2021-12-01 18:06:43 +0300

    3. Интерфейс Input [#181092 #234743]

--- src/main/java/ru/job4j/tracker/Bus.java
+++ src/main/java/ru/job4j/tracker/Bus.java
@@ -3,12 +3,12 @@ package ru.job4j.tracker;
 public class Bus implements  Transport {
     @Override
     public void go() {
-
+        System.out.println("Едет");
     }
 
     @Override
     public void numberOfPassengers(int number) {
-
+        System.out.println("Колличество пассажиров: " + number);
     }
 
     @Override
