31c8bd69df1afaab4afa034752f7db1079f298ee
tigrdima <duys79yandex.ru>
2021-12-06 14:14:27 +0300

    9. Написать тесты на StartUI. [#785 #235136]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
@@ -9,45 +10,53 @@ public class StartUITest {
 
     @Test
     public void whenCreateItem() {
+        Output output = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
+                new String[]{"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
+                new CreateAction(output),
                 new Exit()
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(output).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
 
-   @Test
+    @Test
     public void whenEditItem() {
-        Input in = new StubInput(
-                new String[] {"0", "item", "1", "1", "new item", "2"}
-        );
+        Item item = new Item("test");
         Tracker tracker = new Tracker();
+        tracker.add(item);
+        Output output = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"0", String.valueOf(item.getId()), "new item", "1"}
+        );
+
         UserAction[] actions = {
-                new CreateAction(),
-                new EditItem(),
+                new EditItem(output),
                 new Exit()
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(output).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("new item"));
     }
 
     @Test
     public void whenDeleteItem() {
-        Input in = new StubInput(
-                new String[] {"0", "item", "1", "1", "2"}
-        );
+        Item item = new Item("test");
         Tracker tracker = new Tracker();
+        tracker.add(item);
+        Output output = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"0", String.valueOf(item.getId()), "1"}
+        );
+
         UserAction[] actions = {
-                new CreateAction(),
-                new DeleteItem(),
+                new DeleteItem(output),
                 new Exit()
         };
-        new StartUI().init(in, tracker, actions);
-        assertNull(tracker.findById(1));
+        new StartUI(output).init(in, tracker, actions);
+        assertNull(tracker.findById(item.getId()));
     }
+
 }
