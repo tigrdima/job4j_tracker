0afa48d017a96e0b67fdb4a93c079e10dbbdd9b2
tigrdima <duys79yandex.ru>
2021-12-07 11:01:17 +0300

    change method init

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,7 +12,7 @@ public class StartUI {
         while (run) {
             showMenu(actions);
             int select = input.askInt("Select: ");
-            if (select < 0 || select >= actions.length - 1) {
+            if (select < 0 || select > actions.length - 1) {
                 output.printLn("Wrong input, you can select:  0 .. " + (actions.length - 1));
                 continue;
             }
