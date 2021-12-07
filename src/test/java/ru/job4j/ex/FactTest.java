ba8cd8a81e7922968b7624282d391d3176f64350
tigrdima <duys79yandex.ru>
2021-12-07 09:21:22 +0300

    0.3. Тестирование исключений с junit [#219366 #236570]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class FactTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void whenNGreaterThenNull() {
+    public void whenNlessThanZerol() {
         new Fact().calc(-1);
     }
 }
