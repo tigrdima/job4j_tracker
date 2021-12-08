7ad792c52f1b16b795a76c158ac0c4e19e37019c
tigrdima <duys79yandex.ru>
2021-12-08 15:30:12 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094 #237033]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -10,7 +10,7 @@ public class UsageArrayList {
         names.add("Ivan");
         names.add("Stepan");
 
-        for (Object name : names) {
+        for (String name : names) {
             System.out.println(name);
         }
     }
