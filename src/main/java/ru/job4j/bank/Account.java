21985519fc34aaaea93d61faa05bcc65c7ac2315
tigrdima <duys79yandex.ru>
2021-12-20 18:38:39 +0300

    3. Банковские переводы [#10038 #240712

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -3,32 +3,32 @@ package ru.job4j.bank;
 import java.util.Objects;
 
 public class Account {
-        private String requisite;
-        private double balance;
+    private String requisite;
+    private double balance;
 
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
-        public String getRequisite() {
+    public String getRequisite() {
         return requisite;
     }
 
-        public void setRequisite(String requisite) {
+    public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
-        public double getBalance() {
+    public double getBalance() {
         return balance;
     }
 
-        public void setBalance(double balance) {
+    public void setBalance(double balance) {
         this.balance = balance;
     }
 
-        @Override
-        public boolean equals(Object o) {
+    @Override
+    public boolean equals(Object o) {
         if (this == o) {
             return true;
         }
@@ -39,8 +39,8 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
-        @Override
-        public int hashCode() {
+    @Override
+    public int hashCode() {
         return Objects.hash(requisite);
     }
 
--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -12,13 +12,10 @@ public class BankService {
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
 
-        if (users.containsKey(user)) {
-            for (Map.Entry<User, List<Account>> entry : users.entrySet()) {
-                if (entry.getKey().equals(user)) {
-                    List<Account> userAccount = entry.getValue();
-                    userAccount.add(account);
-                    break;
-                }
+        if (user != null) {
+            List<Account> userAccounts = users.get(user);
+            if (!userAccounts.contains(account)) {
+                userAccounts.add(account);
             }
         }
     }
@@ -36,16 +33,9 @@ public class BankService {
         User user = findByPassport(passport);
 
         if (user != null) {
-            for (Map.Entry<User, List<Account>> entry : users.entrySet()) {
-                User key = entry.getKey();
-                if (key.equals(user)) {
-                    ArrayList<Account> acc = new ArrayList<>(entry.getValue());
-
-                    for (Account userAccount : acc) {
-                        if (userAccount.getRequisite().equals(requisite)) {
-                            return userAccount;
-                        }
-                    }
+            for (Account userAccount : users.get(user)) {
+                if (userAccount.getRequisite().equals(requisite)) {
+                    return userAccount;
                 }
             }
         }
@@ -55,17 +45,13 @@ public class BankService {
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        User user1 = findByPassport(srcPassport);
-        User user2 = findByPassport(destPassport);
 
-        Account accUser1 = findByRequisite(srcPassport, srcRequisite);
-        Account accUser2 = findByRequisite(destPassport, destRequisite);
-        double balUser1 = accUser1.getBalance();
-        double balUser2 = accUser2.getBalance();
+        Account srcAcc = findByRequisite(srcPassport, srcRequisite);
+        Account destAcc = findByRequisite(destPassport, destRequisite);
 
-        if (users.containsKey(user1) && users.containsKey(user2) && balUser1 > balUser2) {
-            accUser1.setBalance(balUser1 - amount);
-            accUser2.setBalance(balUser2 + amount);
+        if (srcAcc != null && destAcc != null && srcAcc.getBalance() >= amount) {
+            srcAcc.setBalance(destAcc.getBalance() - amount);
+            destAcc.setBalance(destAcc.getBalance() + amount);
             rsl = true;
         }
         return rsl;
