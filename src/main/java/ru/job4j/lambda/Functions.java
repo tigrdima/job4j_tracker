2944b7d3cd2ef3d25a8604df6904c6ccb3611950
tigrdima <duys79yandex.ru>
2022-01-05 12:03:44 +0300

    5. Подсчет функции в диапазоне. [#24251 #245380]

--- src/main/java/ru/job4j/lambda/Functions.java
+++ src/main/java/ru/job4j/lambda/Functions.java
@@ -2,19 +2,15 @@ package ru.job4j.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiPredicate;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Functions {
 
     public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> rsl = new ArrayList<>();
-        Consumer<Double> con = rsl::add;
-        BiPredicate<Integer, Integer> pr = (s, e) -> s < e;
 
-        while (pr.test(start, end)) {
-            con.accept(func.apply((double) start++));
+        for (int i = start; i < end; i++) {
+            rsl.add(func.apply((double) i));
         }
         return rsl;
     }
