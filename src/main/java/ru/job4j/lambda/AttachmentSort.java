5d2ddc14cb72014c8392d52fed54baecf1ef058f
tigrdima <duys79yandex.ru>
2022-04-25 17:06:11 +0300

    3. Liquibase. Интеграционные тесты. [#95881 #292913]

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -11,13 +11,10 @@ public class AttachmentSort {
                 new Attachment("image 2", 34),
                 new Attachment("image 3", 13)
         );
-        Comparator comparator =  new Comparator() {
-            @Override
-            public int compare(Object o1, Object o2) {
-                Attachment left = (Attachment) o1;
-                Attachment right = (Attachment) o2;
-                return left.getSize() - right.getSize();
-            }
+        Comparator comparator = (o1, o2) -> {
+            Attachment left = (Attachment) o1;
+            Attachment right = (Attachment) o2;
+            return left.getSize() - right.getSize();
         };
         attachments.sort(comparator);
         System.out.println(attachments);
